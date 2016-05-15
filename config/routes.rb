206ad7938e9a8bca7717dcc000863ac7c2fe032b
Marcle Rodrigues <maarclee@gmail.com>
2016-05-15 00:19:32 -0300

    Applications Resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :applications
   resources :addresses
   resources :skills
   resources :candidates
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515031527) do
+ActiveRecord::Schema.define(version: 20160515031840) do
 
   create_table "addresses", force: :cascade do |t|
     t.text     "street"
@@ -24,6 +24,19 @@ ActiveRecord::Schema.define(version: 20160515031527) do
   end
 
   add_index "addresses", ["candidate_id"], name: "index_addresses_on_candidate_id"
+
+  create_table "applications", force: :cascade do |t|
+    t.integer  "candidate_id"
+    t.integer  "job_id"
+    t.integer  "company_id"
+    t.float    "candidate_match"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "applications", ["candidate_id"], name: "index_applications_on_candidate_id"
+  add_index "applications", ["company_id"], name: "index_applications_on_company_id"
+  add_index "applications", ["job_id"], name: "index_applications_on_job_id"
 
   create_table "candidates", force: :cascade do |t|
     t.text     "first_name"
