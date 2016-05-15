63cea8edbb7b54507a58897a4c092a24aa78e3c6
Marcle Rodrigues <maarclee@gmail.com>
2016-05-14 22:10:30 -0300

    Requirements Resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :requirements
   resources :jobs
   resources :companies
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ spec/factories/requirements.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :requirement do
+    title "MyText"
+    years 1
+    job nil
+  end
+end
