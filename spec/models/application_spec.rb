6585b7fa0539cb0fd5dc34d4ab8f90a6ae37e3e2
Marcle Rodrigues <maarclee@gmail.com>
2016-05-15 00:26:00 -0300

    Applications Specs

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Application, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:company) { FactoryGirl.create(:company) }
+  let(:candidate) { FactoryGirl.create(:candidate) }
+  let(:job) { FactoryGirl.create(:job, company: company)}
+  let(:application) { FactoryGirl.build(:application,
+      candidate: candidate,
+      company: company,
+      job: job
+    )}
+
+  it { is_expected.to belong_to(:candidate) }
+  it { is_expected.to belong_to(:company) }
+  it { is_expected.to belong_to(:job) }
+
+  it 'has a valid factory' do
+    expect(application).to be_valid
+  end
 end
