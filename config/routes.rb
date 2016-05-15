e845b4a8e721f900e751d03a8eeac5ce24772e9e
Marcle Rodrigues <maarclee@gmail.com>
2016-05-14 23:43:08 -0300

    Candidate Resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :candidates
   resources :requirements
   resources :jobs
   resources :companies
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515011009) do
+ActiveRecord::Schema.define(version: 20160515024157) do
+
+  create_table "candidates", force: :cascade do |t|
+    t.text     "first_name"
+    t.text     "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.text     "about"
