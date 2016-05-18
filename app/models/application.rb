3c7fdea7c4831e416a2d839c07723853d78a8ad5
Marcle Rodrigues <maarclee@gmail.com>
2016-05-17 23:12:03 -0300

    Application Model Refactored

--- app/models/application.rb
+++ app/models/application.rb
@@ -9,48 +9,26 @@ class Application < ActiveRecord::Base
     required_list = {}
     skills_list = {}
     requirements.each do |req|
-      req_title = req.title.downcase
-      required_list[req_title] = req.years
+      required_list[req.title.downcase] = req.years
     end
     skills.each do |skill|
-      skill_title = skill.title.downcase
-      skills_list[skill_title] = skill.years.to_f
+      skills_list[skill.title.downcase] = skill.years.to_f
     end
-    required_list = required_list.sort.to_h
-    skills_list = skills_list.sort.to_h
-    skills_that_matter = skills_list.select { |key, value| required_list.include? key }
-    requirements_that_matter = required_list.select { |key, value| skills_list.include? key }
+    skills_that_matter = skills_list.sort.to_h.select { |key, value| required_list.include? key }
+    requirements_that_matter = required_list.sort.to_h.select { |key, value| skills_list.include? key }
     if skills_that_matter == {} or requirements_that_matter == {}
       total = 0
     else
-      total = skills_that_matter.merge(requirements_that_matter) do |key, old_val, new_val|
-        old_val / new_val
-      end
-
-      total = (total.map(&:last).reduce(:+) / requirements.count.to_f)
-
-      if total >= 1.0
-        total = 100
-      else
-        total *= 100
-      end
+      calculate_total(skills_that_matter, requirements_that_matter, requirements.count)
     end
   end
 
-=begin
-  def calculate_match job, candidate
-    requirements = job.requirements.sort.to_h
-    skills = candidate.skills.sort.to_h
-    skills_that_matter = skills.select { |k,v| requirements.include? k}
-    requir_that_matter = requirements.select { |k,v| skills.include? k}
-    total = skills_that_matter.merge(requir_that_matter) do |key, old_val, new_val|
-      old_val / new_val.to_f
-    end.map(&:last).reduce(:+) / requirements.count.to_f
-    if total*100 >= 100
-      total = 100
-    else
-      total*100
-    end
+  private
+
+  def calculate_total(skill, required, requirements_count)
+    total = skill.merge(required) do | key, old_val, new_val |
+      old_val / new_val
+    end.map(&:last).reduce(:+) / requirements_count.to_f
+    total >= 1.0 ? total = 100 : total *= 100
   end
-=end
 end
